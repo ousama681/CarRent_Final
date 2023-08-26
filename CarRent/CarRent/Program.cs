502972baeedc8922d22174dcbd76e3f59166e7d2
Reading Steiner <ousama.askri01@gmail.com>
2023-08-26 07:57:31 +0200

    added swaggerui

--- CarRent/CarRent/Program.cs
+++ CarRent/CarRent/Program.cs
@@ -1,3 +1,6 @@
+using CarRent.CustomerManagement.Domain;
+using CarRent.CustomerManagement.Infrastructure.Persistence;
+
 namespace CarRent
 {
     public class Program
@@ -7,30 +10,32 @@ namespace CarRent
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddDbContext<CustomerContext>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllers();
 
             app.Run();
+
         }
     }
 }
